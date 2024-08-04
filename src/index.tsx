4ca10bf1b33144fa87e9ef996311da4e0392a2f3
Leonid Vilensky <leonid.vilensky@similarweb.com>
2024-08-04 12:41:14 +0300

    Added html, body, root styling 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import App from "./App";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// import * as React from "react";
+// import App from "./App";
+// import { createRoot } from "react-dom/client";
+//
+// createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
